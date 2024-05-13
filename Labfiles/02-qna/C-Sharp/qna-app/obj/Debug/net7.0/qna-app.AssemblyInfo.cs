77648e3b61e428e1eeda0410d097a17cdb1416eb
Gerry O'Brien <gerry.obrien@outlook.com>
2024-05-13 05:51:08 -0700

    added code for labs

--- Labfiles/02-qna/C-Sharp/qna-app/obj/Debug/net7.0/qna-app.AssemblyInfo.cs
+++ Labfiles/02-qna/C-Sharp/qna-app/obj/Debug/net7.0/qna-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qna-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601791943556c973090444717de7500b07236a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a011c6f211ea47b610c2f8bd912b418b5f0813")]
 [assembly: System.Reflection.AssemblyProductAttribute("qna-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qna-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Labfiles/03-language/C-Sharp/clock-client/obj/Debug/net7.0/clock_client.AssemblyInfo.cs
+++ Labfiles/03-language/C-Sharp/clock-client/obj/Debug/net7.0/clock_client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601791943556c973090444717de7500b07236a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a011c6f211ea47b610c2f8bd912b418b5f0813")]
 [assembly: System.Reflection.AssemblyProductAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clock_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
